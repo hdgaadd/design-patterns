21621744a54d03f3bc2d37954ac5d492b03a97a5
CodeChen <53108370+hdgaadd@users.noreply.github.com>
2021-10-10 23:57:46 +0800

    extend

--- src/singleton/EarlySingleton.java
+++ src/singleton/EarlySingleton.java
@@ -1,13 +1,18 @@
 package singleton;
-//懒汉式
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+//饿汉式
 public class EarlySingleton {
-    private static EarlySingleton earlySingleton=new EarlySingleton();
-    private EarlySingleton(){//构造器必须是私有的，否则主程序可通过new EarlySingleton()创造出不同的实例
+    private static EarlySingleton earlySingleton = new EarlySingleton();
+    private EarlySingleton() {//构造器必须是私有的，否则主程序可通过new EarlySingleton()创造出不同的实例
     }
-    public static EarlySingleton getSingleton(){
+    public static EarlySingleton getSingleton() {
         return earlySingleton;
     }
     public static void main(String[] args) {
-        System.out.println("取到该单例为："+EarlySingleton.getSingleton());
+        System.out.println("取到该单例为：" + EarlySingleton.getSingleton());
     }
 }
--- src/singleton/LazilySingleton.java
+++ src/singleton/LazilySingleton.java
@@ -1,12 +1,14 @@
 package singleton;
+
 //懒汉式
+//只能通过类方法来创建对象实例，而不能通过new来获取对象实例
 public class LazilySingleton {
     private static LazilySingleton lazilySingleton;
-    private LazilySingleton(){//构造器必须是私有的，否则主程序可通过new LazilySingleton()创造出不同的实例
+    private LazilySingleton() {//构造器必须是私有的，否则主程序可通过new LazilySingleton()创造出不同的实例
     }
-    public static LazilySingleton getSingleton(){//线程不安全
-        if(lazilySingleton==null)
-            lazilySingleton=new LazilySingleton();
+    public static LazilySingleton getSingleton() {//线程不安全
+        if (lazilySingleton == null)
+            lazilySingleton = new LazilySingleton();
         return lazilySingleton;
     }
     /*public static synchronized LazilySingleton getSingleton(){//线程安全
@@ -15,6 +17,6 @@ public class LazilySingleton {
         return lazilySingleton;
     }*/
     public static void main(String[] args) {
-        System.out.println("取到该单例为："+LazilySingleton.getSingleton());
+        System.out.println("取到该单例为：" + LazilySingleton.getSingleton());
     }
 }
