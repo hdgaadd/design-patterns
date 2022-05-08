220f5e5587dd4271337ae3188fe84001a73d4009
hdgaadd <53108370+hdgaadd@users.noreply.github.com>
2022-05-08 23:58:10 +0800

    composite

---
+++ src/main/java/org/codeman/composite/App.java
@@ -0,0 +1,14 @@
+package org.codeman.composite;
+
+/**
+ * @author hdgaadd
+ * Created on 2022/05/08
+ *
+ * [ˈkɒmpəzɪt]
+ * 组合模式：让接口，统一对待一个对象、或对象的组合
+ */
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
